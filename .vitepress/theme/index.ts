ee4aa06dd3f9ccfa1b38ed0c1f0dc6c845eb1183
ppxb <317842449@qq.com>
2025-10-16 16:38:31 +0800

    pref(ui): 代码块响应式和索引页面样式

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,29 +1,28 @@
-import {h} from 'vue'
-import type {App} from 'vue'
-import Theme from 'vitepress/theme'
-import './custom.css'
+import { h } from "vue";
+import type { App } from "vue";
+import Theme from "vitepress/theme";
+import "./custom.css";
 
-import 'viewerjs/dist/viewer.min.css'
-import imageViewer from 'vitepress-plugin-image-viewer'
+import "viewerjs/dist/viewer.min.css";
+import imageViewer from "vitepress-plugin-image-viewer";
 
-import TeamMembers from './components/TeamMembers.vue'
-import SponsorSection from './components/SponsorSection.vue'
-import {useRoute} from "vitepress";
+import TeamMembers from "./components/TeamMembers.vue";
+import SponsorSection from "./components/SponsorSection.vue";
+import { useRoute } from "vitepress";
 
 export default {
-    ...Theme,
-    setup() {
-        // 获取路由
-        const router = useRoute()
-        // 设置图片查看器
-        imageViewer(router)
-    },
-    Layout: () => {
-        return h(Theme.Layout, null, {
-        })
-    },
-    enhanceApp({app}: {app: App}) {
-        app.component('TeamMembers', TeamMembers)
-        app.component('SponsorSection', SponsorSection)
-    }
-}
+  ...Theme,
+  setup() {
+    // 获取路由
+    const router = useRoute();
+    // 设置图片查看器
+    imageViewer(router);
+  },
+  Layout: () => {
+    return h(Theme.Layout, null, {});
+  },
+  enhanceApp({ app }: { app: App }) {
+    app.component("TeamMembers", TeamMembers);
+    app.component("SponsorSection", SponsorSection);
+  },
+};
